aae50e8e1cfd2b3ee8e307ade67f0a7ed382e8bc
basosa94 <basosa@ucdavis.edu>
2018-06-04 00:37:49 -0700

    Design frontend html pages, write friends.js and server.js

---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+var friends = [
+{
+    "name":"Rachel Green",
+    "photo":"http://images6.fanpop.com/image/photos/34200000/Rachel-Green-rachel-green-34204028-1613-2400.jpg",
+    "scores":[5,1,4,4,5,1,2,5,4,1]
+},
+{
+    "name":"Monica Geller",
+    "photo":"http://images5.fanpop.com/image/photos/25900000/Monica-Geller-monica-geller-25963712-1019-1280.jpg",
+    "scores":[3,3,5,3,4,3,5,4,2,4]
+},
+{
+    "name":"Phoebe Buffay",
+    "photo":"http://images.fanpop.com/images/image_uploads/Phoebe-phoebe-buffay-86994_507_521.jpg",
+    "scores":[4,4,4,4,4,4,4,4,4,4]
+},
+{
+    "name":"Joey Tribbiani",
+    "photo":"http://images2.fanpop.com/image/photos/12400000/Joey-joey-tribbiani-12411488-477-502.jpg",
+    "scores":[3,2,3,4,3,3,2,2,2,2]
+},
+{
+    "name":"Chandler Bing",
+    "photo":"http://images1.fanpop.com/images/photos/1700000/Perry-its-a-bing-thing-1706513-280-350.jpg",
+    "scores":[4,4,1,2,2,3,4,2,1,1]
+},
+{
+    "name":"Ross Geller",
+    "photo":"http://images4.fanpop.com/image/photos/16300000/David-Schwimmer-Ross-Geller-ross-geller-16348282-2115-2560.jpg",
+    "scores":[2,2,2,2,2,2,2,2,2,2]
+}
+]
+
+module.exports = friends;
